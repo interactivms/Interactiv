30f1e67e9030574637ac9eab7c877bb20a54088c
Andre Calderon <andrecalderon@Andres-MacBook-Pro.local>
2022-09-26 18:59:12 -0500

    Corrected useEffect

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -2,15 +2,6 @@ import React, {useEffect, useState} from "react";
 import PageLayout from '../components/layouts/pageLayout'
 export default function Blog() {
 
-  const getPosts = async() => {
-    let res = await getBlogPosts();
-    return res
-  } 
-  useEffect(() => {
-      console.log(getPosts())
-  },[])
-
-
   return <div className="row mx-0 viewh-100 bg-dark"></div>;
 }
 
