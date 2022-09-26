80c7b090129925ef7d69d9b9beb80dfb4f3bd8b7
Andre Calderon <andrecalderon@Andres-MacBook-Pro.local>
2022-09-26 18:53:47 -0500

    Corrected components

--- controllers/mongo.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-const MONGO_OPTIONS = {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-};
-const connectMongo = async () => mongoose.connect(process.env.MONGO_URI).then(() => console.log("Succesfully connected to DB")).catch(err => console.log(err));
-
-export default connectMongo;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import HomeBanner from "../components/HomeBanner";
-import AboutUs from "../components/aboutus";
+import AboutUs from "../components/AboutUs";
 import Footer from "../components/Footer";
 import PageLayout from '../layouts/pageLayout'
 import Services from "../components/Services";
 import BannerParallax from "../components/BannerParallax";
 import OurWork from "../components/OurWork";
-import Testimonials from "../components/Testimonials";
-import Contact from "../components/Contact";
 export default function Home() {
   return (
     <div>
